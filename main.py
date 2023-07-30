dd2dc0c6f8c8934611b8546c5c8e54c95c0db310
threalwinky <voquangvu09112006@gmail.com>
2023-07-30 19:25:04 +0700

    upload files

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ load_dotenv()
 #Create app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "2b3ifbf302f9nc1j2po1jewkajsd"
-app.config['UPLOAD_FOLDER'] = 'static/photoUrl/Users'
+app.config['UPLOAD_FOLDER'] = 'static/photo_url/Users'
 app.config['MAIL_SERVER'] = "smtp.googlemail.com"
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
@@ -30,7 +30,7 @@ app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///user.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
 GOOGLE_CLIENT_ID = "718106880185-vg443k204bnpn8u61fvmlu534dseq11b.apps.googleusercontent.com"
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
 app.permanent_session_lifetime = timedelta(minutes=50)
@@ -50,70 +50,72 @@ flow = Flow.from_client_secrets_file(
 def login_is_required(function):
     def wrapper(*args, **kwargs):
         if "google_id" not in session:
-            return abort(401)  # Authorization required
+            return abort(401)
         else:
             return function()
     return wrapper
 
 #Database model
 class Room(db.Model):
-    room_id = db.Column(db.Integer, primary_key = True)
-    room_name = db.Column(db.String)
-    room_password = db.Column(db.String)
-    photo_Url = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    password = db.Column(db.String)
+    photo_url = db.Column(db.String)
     color = db.Column(db.String)
     participants = db.Column(db.String)
     description = db.Column(db.String)
     chat = db.Column(db.String)
-    def __init__(self, room_name, participants, room_password):
-        self.room_name = room_name
-        self.room_password = room_password
+    def __init__(self, name, participants, password):
+        self.name = name
+        self.password = password
         self.participants = participants
-        self.photo_Url = 'admin.jpg'
+        self.photo_url = 'admin.jpg'
         self.chat = ''
 
 class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-    photoUrl = db.Column(db.String)
+    name = db.Column(db.String)
+    photo_url = db.Column(db.String)
     email = db.Column(db.String)
+    note = db.Column(db.String)
     # chat = db.Column(db.String, nullable=True)
     room = db.Column(db.String)
-    def __init__(self, username, password):
+    def __init__(self, username, password, name):
         self.username = username
         self.password = password
-        self.photoUrl = 'admin.jpg'
+        self.name = name
+        self.photo_url = 'admin.jpg'
         # self.chat = ''
         self.room = ''
 
 #Trailing slash
 @app.route('/', methods=["POST", "GET"])
-def login():
+def log_in():
     if request.method == "POST":
         username = request.form['username']
         username = username.strip()
         password = request.form['password']
-        
+        # password = password.strip()
         found_user = User.query.filter_by(username=username).first()
         if username == '' or password == '':
             flash('Please enter valid username and password', 'info')
-            return redirect(url_for('login'))
+            return redirect(url_for('log_in'))
         if found_user == None:
             flash('Username not found', 'info')
-            return redirect(url_for('login'))
+            return redirect(url_for('log_in'))
         if found_user.password != password:
             flash('Wrong password', 'info')
-            return redirect(url_for('login'))
+            return redirect(url_for('log_in'))
         if found_user.email == None :
             flash("Email not activated please check your email", "info")
-            return redirect(url_for("login"))
+            return redirect(url_for("log_in"))
         session["username"] = username
-        print(username)
         return redirect(url_for('dashboard'))
     if "username" in session:
         return redirect(url_for("dashboard"))
-    return render_template('login.html', session=session)
+    return render_template('log_in.html', session=session)
 
 @app.route("/loginbygoogleaccount")
 def loginbygoogleaccount():
@@ -135,24 +137,22 @@ def callback():
     username = id_info["sub"]
     password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(10))
     found_user = User.query.filter_by(username=username).first()
+    name = id_info["name"]
     if found_user == None:
-        user = User(username, password)
+        user = User(username, password, name)
         user.email = id_info["email"]
         img_data = requests.get(id_info["picture"][:-2] + "0").content
-        print(id_info["picture"])
-        with open('static/photoUrl/Users/' + username + '.jpg', 'wb') as handler:
+        print(id_info)
+        with open('static/photo_url/Users/' + username + '.jpg', 'wb') as handler:
             handler.write(img_data)
         
         db.session.add(user)
         db.session.commit()
-        
-        
+
     found_user = User.query.filter_by(username=username).first()
-    found_user.photoUrl = username + ".jpg"
+    found_user.photo_url = username + ".jpg"
     db.session.commit()
-    return render_template('loginbygoogleaccount.html', username=username, password=found_user.password)
-
-
+    return render_template('log_in_by_google_account.html', username=username, password=found_user.password)
 
 @app.route('/signup', methods=["POST", "GET"])
 def signup():
@@ -169,7 +169,7 @@ def signup():
             flash("Password doesn't match", "info")
             return redirect(url_for("signup"))
         found_user = User.query.filter_by(username=username).first()
-        user = User(username, password)
+        user = User(username, password, username)
         if (found_user == None):
             db.session.add(user)
             db.session.commit()
@@ -180,15 +180,15 @@ def signup():
         # flash(notification)
         # return redirect(url_for("login"))
         sender = "noreply@app.com"
-        msg_title = "EasyLearning: Confirm your email address"
+        msg_title = "Createen: Confirm your email address"
         msg = Message(msg_title, sender=sender, recipients=[email])
         hashemail = sha256(email.encode('utf-8')).hexdigest()
         hash[hashemail] = email
-        msg.body = 'Hello ' + username + """,\nThanks for signing up for EasyLearning!\nPlease click this link to confirm your email address.\n
+        msg.body = 'Hello ' + username + """,\nThanks for signing up for Createen!\nPlease click this link to confirm your email address.
 This means you will be able to reset your password if you forget it later, which is especially important if you have a paid account!
 If you can't click the link from your email program, please copy this URL and paste it into your web browser:
-http://127.0.0.1:5000/confirmEmailSuccess/"""+ username+"/" +hashemail+ """ 
-If you don't want to use EasyLearning, just ignore this message and we won't bother you again.\nCheers,\nThe Create team"""
+http://127.0.0.1:5000/confirmEmailSuccess/"""+ username+"/" + hashemail + """ 
+If you don't want to use Createen, just ignore this message and we won't bother you again.\nCheers,\nThe Create team"""
         data = {
             'username' : username,
             'password' : password,
@@ -196,11 +196,19 @@ If you don't want to use EasyLearning, just ignore this message and we won't bot
         }
         try:
             mail.send(msg)
-            return render_template('confirmEmail.html', data=data)
+            return render_template('confirm_email.html', data=data)
         except Exception as e:
             print(e)
             return "The email was not sent"
     return render_template('signup.html')
+
+@app.route('/resendEmail', methods=["POST", "GET"])
+def resendEmail():
+    return render_template('confirm_email.html')
+
+@app.route('/test')
+def test():
+    return render_template('confirm_email.html')
 
 @app.route('/confirmEmailSuccess/<username>/<email>', methods=["POST", "GET"])
 def confirm_email(username, email):
@@ -210,24 +218,27 @@ def confirm_email(username, email):
     db.session.commit()
     notification = ["Account created! Please log in again", 1]
     flash(notification)
-    return redirect(url_for("login"))
+    return redirect(url_for("log_in"))
 
-@app.route('/logout')
+@app.route('/log_out')
 def log_out():
     session.pop("username", None)
-    return redirect(url_for('login'))
+    return redirect(url_for('log_in'))
 
 @app.route('/dashboard')
 def dashboard():
     if "username" in session:
         username = session["username"]
-        print(session)
-        return render_template('dashboard.html', username=username)
+        found_user = User.query.filter_by(username=username).first()
+        name = found_user.name
+        photo_url = found_user.photo_url
+        email = found_user.email
+        return render_template('dashboard.html', username=username, name=name, photo_url=photo_url, email=email)
     else:
-        return redirect(url_for("login"))
+        return redirect(url_for("log_in"))
 
 #Room Handling
-@app.route('/createRoom', methods=["POST", "GET"])
+@app.route('/create_room', methods=["POST", "GET"])
 def create_room():
     if (request.method == "POST"):
         room_name = request.form["roomname"]
@@ -244,18 +255,18 @@ def create_room():
             found_user.room = found_user.room + "####" + room_id
         db.session.commit()
         return redirect(url_for('join_room'))
-    return render_template('createRoom.html')
+    return render_template('create_room.html')
 
-@app.route('/joinRoom', methods=["POST", "GET"])
+@app.route('/join_room', methods=["POST", "GET"])
 def join_room():
     username = session["username"]
     found_user = User.query.filter_by(username=username).first()
     all_rooms = found_user.room.split("####")
     if all_rooms[0] == '':
-        return render_template('joinRoom.html', all_rooms=all_rooms[1:-1])
-    return render_template('joinRoom.html', all_rooms=all_rooms)
+        return render_template('join_room.html', all_rooms=all_rooms[1:-1])
+    return render_template('join_room.html', all_rooms=all_rooms)
 
-@app.route('/removeRoom', methods=["POST", "GET"])
+@app.route('/remove_room', methods=["POST", "GET"])
 def remove_room():
     username = session["username"]
     if request.method == "POST":
@@ -279,11 +290,10 @@ def remove_room():
                 else:
                     found_room.participants = found_room.participants + "####" + tmp[i]
         # print(found_user.room)
-        
         db.session.commit()
     return "Remove Complete"
 
-@app.route('/addRoom', methods=["POST", "GET"])
+@app.route('/add_room', methods=["POST", "GET"])
 def add_room():
     username = session["username"]
     if request.method == "POST":
@@ -323,9 +333,9 @@ def use_room(room_id):
 
 
 
-@app.route("/individualRoom")
+@app.route("/individual_room")
 def individual_room():
-    return render_template("individualRoom.html")
+    return render_template("individual_room.html")
 
 @app.route("/users")
 def users():
@@ -337,38 +347,40 @@ def rooms():
     user_list = db.session.execute(db.select(Room).order_by(Room.room_name)).scalars()
     return render_template("listRooms.html", users=user_list)
 
-
-
 #Setting up
-@app.route('/settingup', methods=["POST","GET"])
-def settingup():
+@app.route('/setting_up', methods=["POST","GET"])
+def setting_up():
     username = session["username"]
     found_user = User.query.filter_by(username=username).first()
     if request.method == "POST":
+        name = request.form["name"]
+        print(name)
         avatar = request.files["new-avatar"]
         filename = avatar.filename
         arr = ["png", "jpg", "jpeg", "gif", "svg"]
-        if (filename == ''):
-            flash("Không có ảnh nào được gửi lên")
-            return redirect(url_for("settingup"))
-        if (filename.split('.')[1] not in arr):
-            flash("File phải là hình ảnh có đuôi png, jpg, jpeg, gif, svg")
-            return redirect(url_for("settingup"))
-        else :
-            filename = secure_filename(avatar.filename)
-            filename = username + '.' + avatar.filename.rsplit('.')[1]
-            avatar.save(os.path.join(current_app.config.get('UPLOAD_FOLDER'), filename))
-            found_user.photoUrl = filename
-            db.session.commit()
-    return render_template('settingup.html', photoUrl=found_user.photoUrl, username=username)
+        if (filename != ''):
+            # flash("Không có ảnh nào được gửi lên")
+            # return redirect(url_for("setting_up"))
+            if (filename.split('.')[1] not in arr):
+                flash("File phải là hình ảnh có đuôi png, jpg, jpeg, gif, svg")
+                return redirect(url_for("setting_up"))
+            else :
+                filename = secure_filename(avatar.filename)
+                filename = username + '.' + avatar.filename.rsplit('.')[1]
+                avatar.save(os.path.join(current_app.config.get('UPLOAD_FOLDER'), filename))
+                found_user.photo_url = filename
+                
+        found_user.name = name
+        db.session.commit()
+    return render_template('setting_up.html', photo_url=found_user.photo_url, username=username, name=found_user.name, email=found_user.email)
 
-@app.route("/deleteuser/<username>")
-def deleteuser(username):
+@app.route("/delete_user/<username>")
+def delete_user(username):
     found_user = User.query.filter_by(username=username).first()
     session.pop("username", None)
     db.session.delete(found_user)
     db.session.commit()
-    return redirect(url_for("login"))
+    return redirect(url_for("log_in"))
 #Run the app
 if __name__ == "__main__":
     with app.app_context():
