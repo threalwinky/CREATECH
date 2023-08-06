cc54e404e92afd2a63a2d1200648bf97c84c14f0
Leo Adavirat <52106425+LeoAdavirat@users.noreply.github.com>
2023-08-06 22:23:53 +0700

    Update mathscript.py

--- mathscript.py
+++ mathscript.py
@@ -5,5 +5,5 @@ class MathScriptCreaTeen:
     dataset = load_dataset('math_dataset', 'algebra__linear_1d', split='test')
     def __init__(self):
         pass
-    def generate_problem():
-        return MathScriptCreaTeen.dataset[random.randint(0,2000000)]
+    def generate_problem(self):
+        return MathScriptCreaTeen.dataset[random.randint(0,10000)]
