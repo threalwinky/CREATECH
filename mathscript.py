8c78e888396e8a550a483a82503824fce0a368a7
threalwinky <voquangvu09112006@gmail.com>
2023-08-06 18:21:44 -1200

    upload files

--- mathscript.py
+++
@@ -1,9 +0,0 @@
-from datasets import load_dataset
-import random
-
-class MathScriptCreaTeen:
-    dataset = load_dataset('math_dataset', 'algebra__linear_1d', split='test')
-    def __init__(self):
-        pass
-    def generate_problem(self):
-        return MathScriptCreaTeen.dataset[random.randint(0,10000)]
