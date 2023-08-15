547d69c9e8f16ac9a5a507eda4bb1255d17f2122
Leo Adavirat <52106425+LeoAdavirat@users.noreply.github.com>
2023-08-15 17:34:58 +0700

    Update LeoLib.py

--- LeoLib.py
+++ LeoLib.py
@@ -2,13 +2,14 @@ from datasets import load_dataset
 import random
 
 class LeoLib:
-    linear2d = load_dataset("math_dataset","algebra__linear_2d", split="test").shuffle(seed=42)
-    def __init__(self):
-        pass
-    def Algebra__linear_2d():
-        return next(iter(LeoLib.linear2d))
-    
+    linear2d = load_dataset("math_dataset","algebra__linear_2d", split="test")
+    def __init__(self,random_item):
+        self.random_item = random_item
+    def Algebra__linear_2d(random_item = random.randrange(1,10**4)):
+        return [LeoLib.linear2d['question'][random_item],LeoLib.linear2d['answer'][random_item]]
 
 #Usage
 CallLib = LeoLib
+# a = random.randrange(1,10**4)
+# print(CallLib.Algebra__linear_2d(a))
 print(CallLib.Algebra__linear_2d())
